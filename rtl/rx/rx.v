b4dd4bc4c676f4d157522492769135c7acaf1b4e
Omar Mohammed <omarmuayadm@outlook.com>
2024-12-08 16:47:07 +0100

    fixed syntax errors in the reciever

--- rtl/rx/rx.v
+++ rtl/rx/rx.v
@@ -1,144 +1,150 @@
-module rx#(
-  // Input Clock in MHz
-  parameter integer C_CLOCK_FREQ = 100,
+module rx #(
+    // Input Clock in MHz
+    parameter integer C_CLOCK_FREQ = 100,
 
-  // Parameters of Axi Slave Bus Interface S_AXI
-  parameter integer C_S_AXI_DATA_WIDTH	= 32,
-  parameter integer C_S_AXI_ADDR_WIDTH	= 32
+    // Parameters of Axi Slave Bus Interface S_AXI
+    parameter integer C_S_AXI_DATA_WIDTH = 32,
+    parameter integer C_S_AXI_ADDR_WIDTH = 32
 
-)(
-  // Clock, Reset
-  input wire s_axi_aclk,
-  input wire s_axi_aresetn,
-  // Ports of Axi Slave Bus Interface S_AXI
-  input wire [C_S_AXI_ADDR_WIDTH-1 : 0]     s_axi_awaddr,
-  input wire [2 : 0]                        s_axi_awprot,
-  input wire                                s_axi_awvalid,
-  output wire                               s_axi_awready,
-  input wire [C_S_AXI_DATA_WIDTH-1 : 0]     s_axi_wdata,
-  input wire [(C_S_AXI_DATA_WIDTH/8)-1 : 0] s_axi_wstrb,
-  input wire                                s_axi_wvalid,
-  output wire                               s_axi_wready,
-  output wire [1 : 0]                       s_axi_bresp,
-  output wire                               s_axi_bvalid,
-  input wire                                s_axi_bready,
-  input wire [C_S_AXI_ADDR_WIDTH-1 : 0]     s_axi_araddr,
-  input wire [2 : 0]                        s_axi_arprot,
-  input wire                                s_axi_arvalid,
-  output wire                               s_axi_arready,
-  output wire [C_S_AXI_DATA_WIDTH-1 : 0]    s_axi_rdata,
-  output wire [1 : 0]                       s_axi_rresp,
-  output wire                               s_axi_rvalid,
-  input wire                                s_axi_rready,
-  // AXI-Stream - OUTPUT
-  input wire         s_axis_tready,
-  output wire        s_axis_tvalid,
-  output wire        s_axis_sof, // Start of frame
-  output wire [31:0] s_axis_tdata
+) (
+    // Clock, Reset
+    input  wire                                s_axi_aclk,
+    input  wire                                s_axi_aresetn,
+    // Ports of Axi Slave Bus Interface S_AXI
+    input  wire [    C_S_AXI_ADDR_WIDTH-1 : 0] s_axi_awaddr,
+    input  wire [                       2 : 0] s_axi_awprot,
+    input  wire                                s_axi_awvalid,
+    output wire                                s_axi_awready,
+    input  wire [    C_S_AXI_DATA_WIDTH-1 : 0] s_axi_wdata,
+    input  wire [(C_S_AXI_DATA_WIDTH/8)-1 : 0] s_axi_wstrb,
+    input  wire                                s_axi_wvalid,
+    output wire                                s_axi_wready,
+    output wire [                       1 : 0] s_axi_bresp,
+    output wire                                s_axi_bvalid,
+    input  wire                                s_axi_bready,
+    input  wire [    C_S_AXI_ADDR_WIDTH-1 : 0] s_axi_araddr,
+    input  wire [                       2 : 0] s_axi_arprot,
+    input  wire                                s_axi_arvalid,
+    output wire                                s_axi_arready,
+    output wire [    C_S_AXI_DATA_WIDTH-1 : 0] s_axi_rdata,
+    output wire [                       1 : 0] s_axi_rresp,
+    output wire                                s_axi_rvalid,
+    input  wire                                s_axi_rready,
+    // AXI-Stream - OUTPUT
+    output wire                                s_axis_tready,
+    input  wire                                s_axis_tvalid,
+    input  wire                                s_axis_sof,     // Start of frame
+    input  wire                                s_axis_eof,     // Start of frame
+    input  wire [                        31:0] s_axis_tdata
 );
 
 
-// **Wires
-wire [31:0] w_prbs_out;
-wire [31:0] w_reg_prbs_seed;
-wire        w_prbs_start;
+  // **Wires
+  wire [31:0] w_prbs_out;
+  wire [31:0] w_reg_prbs_seed;
+  wire        w_reg_rx_enable;
+  wire        w_prbs_start;
 
-// **Registers
-reg        r_axis_tready;
-reg [31:0] r_rx_data;
-reg        r_rx_sof;
-reg        r_tx_eof;
+  // **Registers
+  reg         r_axis_tready;
+  reg         r_reg_rx_enable_d;
+  reg  [31:0] r_rx_data;
+  reg         r_rx_sof;
+  reg         r_rx_eof;
+  reg  [31:0] r_prbs_sync_check;
 
-// Assignment
-assign s_axis_tready = r_axis_tready;
-
-assign w_prbs_start = s_axis_tvalid && r_reg_rx_enable_d;
-
-
-// Instantiation of Axi Bus Interface S_AXI
-rx_axi_slave #(
-  .C_S_AXI_DATA_WIDTH(C_S_AXI_DATA_WIDTH),
-  .C_S_AXI_ADDR_WIDTH(C_S_AXI_ADDR_WIDTH)
-) rx_axi_slave_inst (
-  // Control Registers
-  .o_reg_prbs_seed (w_reg_prbs_seed),
-  // AXI interface
-  .S_AXI_ACLK       (s_axi_aclk),
-  .S_AXI_ARESETN    (s_axi_aresetn),
-  .S_AXI_AWADDR     (s_axi_awaddr),
-  .S_AXI_AWPROT     (s_axi_awprot),
-  .S_AXI_AWVALID    (s_axi_awvalid),
-  .S_AXI_AWREADY    (s_axi_awready),
-  .S_AXI_WDATA      (s_axi_wdata),
-  .S_AXI_WSTRB      (s_axi_wstrb),
-  .S_AXI_WVALID     (s_axi_wvalid),
-  .S_AXI_WREADY     (s_axi_wready),
-  .S_AXI_BRESP      (s_axi_bresp),
-  .S_AXI_BVALID     (s_axi_bvalid),
-  .S_AXI_BREADY     (s_axi_bready),
-  .S_AXI_ARADDR     (s_axi_araddr),
-  .S_AXI_ARPROT     (s_axi_arprot),
-  .S_AXI_ARVALID    (s_axi_arvalid),
-  .S_AXI_ARREADY    (s_axi_arready),
-  .S_AXI_RDATA      (s_axi_rdata),
-  .S_AXI_RRESP      (s_axi_rresp),
-  .S_AXI_RVALID     (s_axi_rvalid),
-  .S_AXI_RREADY     (s_axi_rready)
-);
-
-prbs prbs_inst(
-  .i_aclk     (s_axi_aclk),
-  .i_aresetn  (s_axi_aresetn),
-  .i_start    (s_axis_tvalid),
-  .i_prbs_seed(w_prbs_start),
-  .o_prbs     (w_prbs_out)
-);
+  // Assignment
+  assign s_axis_tready = r_axis_tready;
+  assign w_prbs_start  = s_axis_tvalid && r_reg_rx_enable_d;
 
 
-// TREADY Enable
-always @(posedge s_axi_aclk or negedge s_axi_aresetn) begin
-  if(s_axi_aresetn == 1'b0) begin
-    r_axis_tready <= 1'b0;
-    //msh
-    r_reg_rx_enable_d <= 1'b0;
-  end
-  else begin
-    // msh
-    r_reg_rx_enable_d <= r_reg_rx_enable;
+  // -----------------------
+  // Instantiation of Axi Bus Interface S_AXI
+  // -----------------------
+  rx_axi_slave #(
+      .C_S_AXI_DATA_WIDTH(C_S_AXI_DATA_WIDTH),
+      .C_S_AXI_ADDR_WIDTH(C_S_AXI_ADDR_WIDTH)
+  ) rx_axi_slave_inst (
+      // Registers
+      .o_reg_rx_enable(w_reg_rx_enable),
+      .o_reg_prbs_seed(w_reg_prbs_seed),
+      // AXI Bus
+      .S_AXI_ACLK     (s_axi_aclk),
+      .S_AXI_ARESETN  (s_axi_aresetn),
+      .S_AXI_AWADDR   (s_axi_awaddr),
+      .S_AXI_AWPROT   (s_axi_awprot),
+      .S_AXI_AWVALID  (s_axi_awvalid),
+      .S_AXI_AWREADY  (s_axi_awready),
+      .S_AXI_WDATA    (s_axi_wdata),
+      .S_AXI_WSTRB    (s_axi_wstrb),
+      .S_AXI_WVALID   (s_axi_wvalid),
+      .S_AXI_WREADY   (s_axi_wready),
+      .S_AXI_BRESP    (s_axi_bresp),
+      .S_AXI_BVALID   (s_axi_bvalid),
+      .S_AXI_BREADY   (s_axi_bready),
+      .S_AXI_ARADDR   (s_axi_araddr),
+      .S_AXI_ARPROT   (s_axi_arprot),
+      .S_AXI_ARVALID  (s_axi_arvalid),
+      .S_AXI_ARREADY  (s_axi_arready),
+      .S_AXI_RDATA    (s_axi_rdata),
+      .S_AXI_RRESP    (s_axi_rresp),
+      .S_AXI_RVALID   (s_axi_rvalid),
+      .S_AXI_RREADY   (s_axi_rready)
+  );
 
-    // Enable AXI-Stream if reciever is enabled
-    if (w_reg_rx_enable_d == 1'b1) begin
-      r_axis_tready <= 1'b1;
+  // -----------------------
+  // PRBS Generator
+  // -----------------------
+  prbs prbs_inst (
+      .i_aclk     (s_axi_aclk),
+      .i_aresetn  (s_axi_aresetn),
+      .i_start    (w_prbs_start),
+      .i_prbs_seed(w_reg_prbs_seed),
+      .o_prbs     (w_prbs_out)
+  );
+
+
+  // TREADY Enable
+  always @(posedge s_axi_aclk or negedge s_axi_aresetn) begin
+    if (s_axi_aresetn == 1'b0) begin
+      r_axis_tready <= 1'b0;
+      //msh
+      r_reg_rx_enable_d <= 1'b0;
+    end else begin
+      // msh
+      r_reg_rx_enable_d <= w_reg_rx_enable;
+
+      // Enable AXI-Stream if reciever is enabled
+      if (r_reg_rx_enable_d == 1'b1) begin
+        r_axis_tready <= 1'b1;
+      end
     end
   end
-end
 
-// Capture AXI-Stream Data
-always @(posedge s_axi_aclk or negedge s_axi_aresetn) begin
-  if (s_axi_aresetn == 1'b0) begin
-    r_rx_data <= 32'd0;
-    r_rx_sof <= 1'b0;
-    r_rx_eof <= 1'b0;
-  end
-  else begin
-    // Check that tvalid is set before capturing data
-    if (r_axis_tready == 1'b1 && s_axis_tvalid == 1'b1) begin
-      r_rx_data <= s_axis_tdata;
-      r_rx_sof  <= s_axis_sof;
-      r_rx_eof  <= s_axis_eof;
+  // Capture AXI-Stream Data
+  always @(posedge s_axi_aclk or negedge s_axi_aresetn) begin
+    if (s_axi_aresetn == 1'b0) begin
+      r_rx_data <= 32'd0;
+      r_rx_sof  <= 1'b0;
+      r_rx_eof  <= 1'b0;
+    end else begin
+      // Check that tvalid is set before capturing data
+      if (r_axis_tready == 1'b1 && s_axis_tvalid == 1'b1) begin
+        r_rx_data <= s_axis_tdata;
+        r_rx_sof  <= s_axis_sof;
+        r_rx_eof  <= s_axis_eof;
+      end
     end
   end
-  
 
   // NOTE: This logic is used to check if the reciever and transmitter is in
   // sync
   always @(posedge s_axi_aclk or negedge s_axi_aresetn) begin
     if (s_axi_aresetn == 1'b0) begin
       r_prbs_sync_check <= 32'd0;
-    end
-    else begin
+    end else begin
       if (r_reg_rx_enable_d == 1'b1) begin
-        r_prbs_sync_check <= r_rx_data ^ r_prbs_out;
+        r_prbs_sync_check <= r_rx_data ^ w_prbs_out;
       end
     end
   end
--- tb/tb_fpga.sv
+++ tb/tb_fpga.sv
@@ -8,140 +8,180 @@
 // 123:   - PRBS Transmitter
 // -----------------------------------------
 
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
 `include "interfaces.sv"
 
-module tb_fpga(
-    input wire          s_axi_aclk,
-    input wire          s_axi_aresetn,
+module tb_fpga (
+    input wire s_axi_aclk,
+    input wire s_axi_aresetn,
 
     // Transmitter. AXI-Stream Output
-    input wire         m_axis_tready,
+    output wire        m_axis_tready,
     output wire        m_axis_tvalid,
     output wire        m_axis_sof,
     output wire [31:0] m_axis_tdata,
 
 
     // Ports of Axi Slave Bus Interface S00_AXI
-    input wire [31 : 0]       s_axi_awaddr,
-    input wire [2 : 0]        s_axi_awprot,
-    input wire                s_axi_awvalid,
-    output wire               s_axi_awready,
-    input wire [31 : 0]       s_axi_wdata,
-    input wire [(32/8)-1 : 0] s_axi_wstrb,
-    input wire                s_axi_wvalid,
-    output wire               s_axi_wready,
-    output wire [1 : 0]       s_axi_bresp,
-    output wire               s_axi_bvalid,
-    input wire                s_axi_bready,
-    input wire [32-1 : 0]     s_axi_araddr,
-    input wire [2 : 0]        s_axi_arprot,
-    input wire                s_axi_arvalid,
-    output wire               s_axi_arready,
-    output wire [32-1 : 0]    s_axi_rdata,
-    output wire [1 : 0]       s_axi_rresp,
-    output wire               s_axi_rvalid,
-    input wire                s_axi_rready
+    input  wire [      31 : 0] s_axi_awaddr,
+    input  wire [       2 : 0] s_axi_awprot,
+    input  wire                s_axi_awvalid,
+    output wire                s_axi_awready,
+    input  wire [      31 : 0] s_axi_wdata,
+    input  wire [(32/8)-1 : 0] s_axi_wstrb,
+    input  wire                s_axi_wvalid,
+    output wire                s_axi_wready,
+    output wire [       1 : 0] s_axi_bresp,
+    output wire                s_axi_bvalid,
+    input  wire                s_axi_bready,
+    input  wire [    32-1 : 0] s_axi_araddr,
+    input  wire [       2 : 0] s_axi_arprot,
+    input  wire                s_axi_arvalid,
+    output wire                s_axi_arready,
+    output wire [    32-1 : 0] s_axi_rdata,
+    output wire [       1 : 0] s_axi_rresp,
+    output wire                s_axi_rvalid,
+    input  wire                s_axi_rready
 );
 
-// **Parameters
-localparam NUM_AXI_INTF = 2;
+  // **Parameters
+  localparam integer NUM_AXI_INTF = 2;
 
-// **Wires
-
-
-axi_if axi_s();
-axi_if axi_m[NUM_AXI_INTF]();
-
-// **Registers
+  // **Wires
 
 
-// DUT
-// INDEX: Componennts
+  axi_if axi_s ();
+  axi_if axi_m[NUM_AXI_INTF] ();
 
-// --------------------------------------
-// INDEX: ___
-// INDEX: Zynq FPGA
-// INDEX:   - AXI Interconnect
-// Description: Covnert SPI data to AXI Stream data
-// --------------------------------------
-    assign axi_s.awaddr  = s_axi_awaddr;
-    assign axi_s.awprot  = s_axi_awprot;
-    assign axi_s.awvalid = s_axi_awvalid;
-
-    assign s_axi_awready = axi_s.awready;
-
-    assign axi_s.wdata   = s_axi_wdata;
-    assign axi_s.wstrb   = s_axi_wstrb;
-    assign axi_s.wvalid  = s_axi_wvalid;
-
-    assign s_axi_wready  = axi_s.wready;
-    assign s_axi_bresp   = axi_s.bresp;
-    assign s_axi_bvalid  = axi_s.bvalid;
-
-    assign axi_s.bready  = s_axi_bready;
-    assign axi_s.araddr  = s_axi_araddr;
-    assign axi_s.arprot  = s_axi_arprot;
-    assign axi_s.arvalid = s_axi_arvalid;
-
-    assign s_axi_arready = axi_s.arready;
-    assign s_axi_rdata   = axi_s.rdata;
-    assign s_axi_rresp   = axi_s.rresp;
-    assign s_axi_rvalid  = axi_s.rvalid;
-
-    assign axi_s.rready  = s_axi_rready;
-
-axi_interconnect #(
-    .NUM_AXI_INTF(NUM_AXI_INTF)
-)axi_interconnect_inst (
-    // AXI-Slave S00 sim_top.c
-    .axi_s(axi_s.slave),
-    // AXI-Msater M00
-    .axi_m(axi_m)
-);
+  // **Registers
 
 
-// --------------------------------------
-// INDEX: ___
-// INDEX:   - PRBS Transmitter
-// Description: TTL interface
-// --------------------------------------
-tx #
-(
-    // Parameters of Axi Slave Bus Interface S_AXI
-    .C_S_AXI_DATA_WIDTH(32),
-    .C_S_AXI_ADDR_WIDTH(32)
-) tx_inst (
+  // DUT
+  // INDEX: Componennts
 
-    // Ports of Axi Slave Bus Interface S_AXI
-    .s_axi_aclk    (s_axi_aclk),
-    .s_axi_aresetn (s_axi_aresetn),
-    .s_axi_awaddr  (axi_m[0].awaddr ),
-    .s_axi_awprot  (axi_m[0].awprot ),
-    .s_axi_awvalid (axi_m[0].awvalid),
-    .s_axi_awready (axi_m[0].awready),
-    .s_axi_wdata   (axi_m[0].wdata  ),
-    .s_axi_wstrb   (axi_m[0].wstrb  ),
-    .s_axi_wvalid  (axi_m[0].wvalid ),
-    .s_axi_wready  (axi_m[0].wready ),
-    .s_axi_bresp   (axi_m[0].bresp  ),
-    .s_axi_bvalid  (axi_m[0].bvalid ),
-    .s_axi_bready  (axi_m[0].bready ),
-    .s_axi_araddr  (axi_m[0].araddr ),
-    .s_axi_arprot  (axi_m[0].arprot ),
-    .s_axi_arvalid (axi_m[0].arvalid),
-    .s_axi_arready (axi_m[0].arready),
-    .s_axi_rdata   (axi_m[0].rdata  ),
-    .s_axi_rresp   (axi_m[0].rresp  ),
-    .s_axi_rvalid  (axi_m[0].rvalid ),
-    .s_axi_rready  (axi_m[0].rready ),
+  // --------------------------------------
+  // INDEX: ___
+  // INDEX: Zynq FPGA
+  // INDEX:   - AXI Interconnect
+  // Description: Covnert SPI data to AXI Stream data
+  // --------------------------------------
+  assign axi_s.awaddr  = s_axi_awaddr;
+  assign axi_s.awprot  = s_axi_awprot;
+  assign axi_s.awvalid = s_axi_awvalid;
 
-    // AXI-Stream - OUTPUT
-    .m_axis_tready (m_axis_tready),
-    .m_axis_tvalid (m_axis_tvalid),
-    .m_axis_sof    (m_axis_sof), // Start of frame
-    .m_axis_tdata  (m_axis_tdata)
-);
+  assign s_axi_awready = axi_s.awready;
+
+  assign axi_s.wdata   = s_axi_wdata;
+  assign axi_s.wstrb   = s_axi_wstrb;
+  assign axi_s.wvalid  = s_axi_wvalid;
+
+  assign s_axi_wready  = axi_s.wready;
+  assign s_axi_bresp   = axi_s.bresp;
+  assign s_axi_bvalid  = axi_s.bvalid;
+
+  assign axi_s.bready  = s_axi_bready;
+  assign axi_s.araddr  = s_axi_araddr;
+  assign axi_s.arprot  = s_axi_arprot;
+  assign axi_s.arvalid = s_axi_arvalid;
+
+  assign s_axi_arready = axi_s.arready;
+  assign s_axi_rdata   = axi_s.rdata;
+  assign s_axi_rresp   = axi_s.rresp;
+  assign s_axi_rvalid  = axi_s.rvalid;
+
+  assign axi_s.rready  = s_axi_rready;
+
+  axi_interconnect #(
+      .NUM_AXI_INTF(NUM_AXI_INTF)
+  ) axi_interconnect_inst (
+      // AXI-Slave S00 sim_top.c
+      .axi_s(axi_s.slave),
+      // AXI-Msater M00
+      .axi_m(axi_m)
+  );
+
+
+  // --------------------------------------
+  // INDEX: ___
+  // INDEX:   - PRBS Transmitter
+  // Description: TTL interface
+  // --------------------------------------
+  tx #(
+      // Parameters of Axi Slave Bus Interface S_AXI
+      .C_S_AXI_DATA_WIDTH(32),
+      .C_S_AXI_ADDR_WIDTH(32)
+  ) tx_inst (
+
+      // Ports of Axi Slave Bus Interface S_AXI
+      .s_axi_aclk   (s_axi_aclk),
+      .s_axi_aresetn(s_axi_aresetn),
+      .s_axi_awaddr (axi_m[0].awaddr),
+      .s_axi_awprot (axi_m[0].awprot),
+      .s_axi_awvalid(axi_m[0].awvalid),
+      .s_axi_awready(axi_m[0].awready),
+      .s_axi_wdata  (axi_m[0].wdata),
+      .s_axi_wstrb  (axi_m[0].wstrb),
+      .s_axi_wvalid (axi_m[0].wvalid),
+      .s_axi_wready (axi_m[0].wready),
+      .s_axi_bresp  (axi_m[0].bresp),
+      .s_axi_bvalid (axi_m[0].bvalid),
+      .s_axi_bready (axi_m[0].bready),
+      .s_axi_araddr (axi_m[0].araddr),
+      .s_axi_arprot (axi_m[0].arprot),
+      .s_axi_arvalid(axi_m[0].arvalid),
+      .s_axi_arready(axi_m[0].arready),
+      .s_axi_rdata  (axi_m[0].rdata),
+      .s_axi_rresp  (axi_m[0].rresp),
+      .s_axi_rvalid (axi_m[0].rvalid),
+      .s_axi_rready (axi_m[0].rready),
+
+      // AXI-Stream - OUTPUT encrypted data
+      .m_axis_tready(m_axis_tready),
+      .m_axis_tvalid(m_axis_tvalid),
+      .m_axis_sof   (m_axis_sof),     // Start of frame
+      .m_axis_tdata (m_axis_tdata)
+  );
+
+  // --------------------------------------
+  // INDEX: ___
+  // INDEX:   - PRBS Transmitter
+  // Description: TTL interface
+  // --------------------------------------
+  rx #(
+      // Parameters of Axi Slave Bus Interface S_AXI
+      .C_S_AXI_DATA_WIDTH(32),
+      .C_S_AXI_ADDR_WIDTH(32)
+  ) rx_inst (
+
+      // Ports of Axi Slave Bus Interface S_AXI
+      .s_axi_aclk   (s_axi_aclk),
+      .s_axi_aresetn(s_axi_aresetn),
+      .s_axi_awaddr (axi_m[1].awaddr),
+      .s_axi_awprot (axi_m[1].awprot),
+      .s_axi_awvalid(axi_m[1].awvalid),
+      .s_axi_awready(axi_m[1].awready),
+      .s_axi_wdata  (axi_m[1].wdata),
+      .s_axi_wstrb  (axi_m[1].wstrb),
+      .s_axi_wvalid (axi_m[1].wvalid),
+      .s_axi_wready (axi_m[1].wready),
+      .s_axi_bresp  (axi_m[1].bresp),
+      .s_axi_bvalid (axi_m[1].bvalid),
+      .s_axi_bready (axi_m[1].bready),
+      .s_axi_araddr (axi_m[1].araddr),
+      .s_axi_arprot (axi_m[1].arprot),
+      .s_axi_arvalid(axi_m[1].arvalid),
+      .s_axi_arready(axi_m[1].arready),
+      .s_axi_rdata  (axi_m[1].rdata),
+      .s_axi_rresp  (axi_m[1].rresp),
+      .s_axi_rvalid (axi_m[1].rvalid),
+      .s_axi_rready (axi_m[1].rready),
+
+      // AXI-Stream - INPUT encrypted data
+      .s_axis_tready(m_axis_tready),
+      .s_axis_tvalid(m_axis_tvalid),
+      .s_axis_sof   (m_axis_sof),     // Start of frame
+      .s_axis_tdata (m_axis_tdata)
+  );
 
 endmodule
